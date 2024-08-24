d177be293bc73138510eba1424333424b4fbae8b
Yuto Nagano <chamelion@hotmail.co.jp>
2024-08-24 03:56:06 +0000

    ローカル環境でビルドできるかどうかを確認

--- pipeline/main.tf
+++ pipeline/main.tf
@@ -52,12 +52,11 @@ resource "aws_iam_role" "codebuild_role" {
 
 resource "aws_codebuild_project" "serverless_app_build" {
   name          = "${var.env_code}-serverless-app-build"
-  build_timeout = 5
   service_role  = aws_iam_role.codebuild_role.arn
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = "true"
