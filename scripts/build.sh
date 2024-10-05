774b3de6a1d781c859d407dcba8d191ef0d4b114
Yuto Nagano <chamelion@hotmail.co.jp>
2024-10-05 17:29:26 +0900

    ビルドスクリプトをcheck/testに分割

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# cd ${SCRIPT_DIR}/../frontend && npm run test
 cd ${SCRIPT_DIR}/../frontend && npm run build
 cd ${SCRIPT_DIR}/../bff/auth && poetry build-lambda
 cd ${SCRIPT_DIR}/../api/hello && poetry build-lambda
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,5 +4,6 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export SA_FRONTEND_URL=$(cd ${SCRIPT_DIR}/../terraform && terraform output frontend_domain_name | tr -d '"')
-cd ${SCRIPT_DIR}/../gauge && TAIKO_BROWSER_ARGS=--no-sandbox,--start-maximized,--disable-dev-shm-usage npm run test
+# cd ${SCRIPT_DIR}/../frontend && npm run test
+cd ${SCRIPT_DIR}/../bff/auth && poetry run pytest --junitxml=unittests.xml --cov-report=xml:coverage.xml --cov=./auth
+cd ${SCRIPT_DIR}/../api/hello && poetry run pytest --junitxml=unittests.xml --cov-report=xml:coverage.xml --cov=./hello
